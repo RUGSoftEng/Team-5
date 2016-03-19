8210ac12f30ed74d1024e959de1c145192ba5b56
Sander de Jong <s.de.jong.24@student.rug.nl>
2016-03-19 16:57:53 +0100

    Added tab option, bugged

--- app/form.js
+++ app/form.js
@@ -8,21 +8,28 @@
  */
 
 define(['app/database', 'jquery'], function (db, $) {
-  var numberOfFormItems = 2;
-  $(".add").click(function() {
-    $('.itemform > :nth-child(2)').clone(true).insertBefore(".itemform > p:last-child");
-    numberOfFormItems++;
-    return false;
-  });
+	 var numberOfFormItems = 2;
 
-  $(".remove").click(function() {
-  	if (numberOfFormItems != 2) {
-  		$(this).parent().remove();	
-  		numberOfFormItems--;
-  	}
-  	
+	 $(".add").click(function(add) {
+	   $('.itemform > :nth-child(2)').clone(true).insertBefore(".itemform > p:last-child");
+	   numberOfFormItems++;
+	   return false;
+	 });
 
-  });	
+	 $(".remove").click(function() {
+	  if (numberOfFormItems != 2) {
+	  		$(this).parent().remove();	
+	  	numberOfFormItems--;
+	  	}
+	 });
+
+ 	if (document.activeElement == ($('.itemform').children[(numberOfFormItems-1)].children[5])) {
+	   $('#hint').keydown(function (e) {
+	   	if (e.keyCode == 9) {
+	  		console.log("gfgdg");
+	  	}
+	  });
+ 	};  	
 });
 
 
