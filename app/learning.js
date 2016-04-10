2e8d21ec38dd8e23dd4f9e4c1177dde2473ff19f
sibrenvanvliet <s.van.vliet.4@student.rug.nl>
2016-04-10 11:52:11 +0200

    Put disableAutocomplete into function

--- app/learning.js
+++ app/learning.js
@@ -10,10 +10,25 @@
 define(['jquery', 'bootstrap', 'app/config', 'app/database', 'app/messages', 'app/question', 'app/timer','app/database'], function ($, bootstrap, config, db, messages, question, timer,db) {
   timer.startTimer(".timer", config.constant("TIME_LIMIT"));
   var waitingForEnter = false;
+  
+  function disableAutocomplete() {
+    $('input').attr('autocomplete', 'off');
+  }
+  
+  function nextQuestion() {
+    clearTimeout(timeout);
+    timer.clearCountdown();
+    messages.clear();
+    $( "#answer" ).prop("disabled", false);
+    $( "#answer" ).val( "" );
+    $( "#answer" ).focus();
+    question.show();
+    waitingForEnter = false;
+  }
 
-  // Disable autocomplete that provides suggestions when typing words
-  $('input').attr('autocomplete', 'off');
-//when the page is loaded we get the datasetId from the page url and load the dataset from the databese
+  disableAutocomplete();
+  
+  //when the page is loaded we get the datasetId from the page url and load the dataset from the databese
   $(document).ready(function() {
     var url =   window.location.href
     var datasetId = url.substring(url.indexOf('?')+1);
@@ -42,15 +57,4 @@ define(['jquery', 'bootstrap', 'app/config', 'app/database', 'app/messages', 'ap
       }
   	}
   });
-
-  function nextQuestion() {
-    clearTimeout(timeout);
-    timer.clearCountdown();
-    messages.clear();
-    $( "#answer" ).prop("disabled", false);
-    $( "#answer" ).val( "" );
-    $( "#answer" ).focus();
-    question.show();
-    waitingForEnter = false;
-  }
 });
