d8252fb830016ca10673486c3024cb1cd5c5996e
sibrenvanvliet <s.van.vliet.4@student.rug.nl>
2016-04-22 13:45:45 +0200

    Made mockup version of login page

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow () {
   mainWindow.maximize();
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/login.html');
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
