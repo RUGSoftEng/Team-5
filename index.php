bef2ed84eb55f27d9a6844fd4cace1701ba83e9f
Enrico Werkema <enricowerkema@gmail.com>
2016-05-22 16:49:45 +0200

    add index page

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>CodeCentral Basing</title>
+		<meta name="description" content="Description">
+		<meta name="author" content="Code Central">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="shortcut icon" href="images/favicon.png"/>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="bootstrap.css">
+		<script src="resources/js/jquery-1.11.1.min.js"></script>
+		<script src="resources/js/bootstrap.min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,600' rel='stylesheet' type='text/css'>
+		<script type="text/javascript">
+			$(document).ready(function () {
+					
+			});
+		</script>
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<header>
+			<div class="container navbar">
+				<h1 id="logo" class='navbar-brand'><a href="#">Upgrade Groningen</a></h1>
+				
+				<button class="navbar-toggle" data-toggle='collapse' data-target='.navHeaderCollapse'>
+					<h2>Menu</h2>
+					<div class="icons">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</div>
+					<div class="clear"></div>
+				</button>
+				<nav class='collapse navbar-collapse navHeaderCollapse'> 
+					<ul class='nav navbar-nav navbar-right'>
+						<li class="first selected"><a href="#">Home</a></li>
+						<li class="has-submenu dropdown visible-xs-block">
+							<a href="#" class='dropdown-toggle' data-toggle='dropdown' data-target=''>Mijn account</a>
+							<ul class="submenu dropdown-menu">
+								<li><a href="#">Mijn gegevens</a></li>
+								<li><a href="#">Leerling gegevens</a></li>
+								<li class="last"><a href="#">Bijlespaketten</a></li>
+							</ul>
+						</li>
+						<li class="has-submenu dropdown">
+							<a href="#" class='dropdown-toggle' data-toggle='dropdown' data-target=''>Pre-examentraining</a>
+							<ul class="submenu dropdown-menu">
+								<li><a href="#">Examentrainingen</a></li>
+								<li><a href="#">Oude examens</a></li>
+								<li class="last"><a href="#">Aanmelden</a></li>
+							</ul>
+						</li>
+						<li class='hidden-xs'><a href="#">Mijn account</a></li>
+						
+						<li><a href="#">Upgrade</a></li>
+						<li class='visible-xs-block'><a href="#">Contact</a></li>
+					</ul>	
+				</nav>
+				
+				<div class="clear"></div>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<div id="content">
+					
+				</div>
+				<aside>
+					
+				</aside>
+				<div class="clear"></div>
+			</div>
+		</main>
+		<footer>
+			<div class="container">
+				
+			</div>
+		</footer>
+	</body>
+</html>
