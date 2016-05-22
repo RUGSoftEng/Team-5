a548a9daca1e6db69c97ab5a8a9a7752f72ad98b
Enrico Werkema <enricowerkema@gmail.com>
2016-05-22 18:31:45 +0200

    UTC standard, now really

--- app/date.js
+++ app/date.js
@@ -1,7 +1,7 @@
 define(["dateformat"], function (dateFormat) {
     return {
         dateToDatetime: function(date) {
-            return dateFormat(date, "yyyy-mm-dd HH:mm:ss");
+            return dateFormat(date, "yyyy-mm-dd HH:mm:ss", true);
         },
         format: function(val, options) {
           return dateFormat(val, options, true);
